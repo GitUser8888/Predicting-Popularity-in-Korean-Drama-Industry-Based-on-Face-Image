21e9a92a0952b6deb73c2c3c701ab68d414cf3ac
GitUser8888 <125370946+GitUser8888@users.noreply.github.com>
2023-08-02 21:11:08 +0800

    Update app_efficientnet_autoencoder.py

--- streamlit/app_efficientnet_autoencoder.py
+++ streamlit/app_efficientnet_autoencoder.py
@@ -28,7 +28,7 @@ base_model_efficientnet = EfficientNetB0(weights='imagenet', include_top=False)
 # Use the trained encoder to reduce the dimensionality of the features
 @st.cache_resource
 def load_encoder():
-    h5_encoder = load_keras_model('EfficientNet_BatchSize512_50epochs_testsize_0.2encoder.h5')
+    h5_encoder = load_keras_model('https://raw.githubusercontent.com/GitUser8888/Predicting-Popularity-in-Korean-Drama-Industry-Based-on-Face-Image/main/streamlit/EfficientNet_BatchSize512_50epochs_testsize_0.2encoder.h5')
     return h5_encoder
 
 encoder = load_encoder()
